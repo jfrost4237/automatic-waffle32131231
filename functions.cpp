3ad7a8a0169f83668368f36507619a81530e0726
jfrost4237 <77891978+jfrost4237@users.noreply.github.com>
2022-12-07 18:35:02 +0100

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -3,19 +3,34 @@
 
 
 void Start() {
+    
+        std::cout << "enter file name: ";
+        std::cin >> file;
+        std::cout << "\n";
+    
 
+    int choice = 0;
+    std::cout << "1 = Enter grades\n2 = Print table of grades\n3 = Exit\n\nenter choice: ";
+    std::cin >> choice;
 
-    std::cout << "enter file name: ";
-    std::cin >> file;
-    std::cout << "\n";
+    switch (choice) {
+    case 1: start_grading();
+        break;
+    case 2: {
+        std::cout << "\nWhich subject would you like to enter?\n";
+        std::cin >> subject;
+       
+        std::transform(subject.begin(), subject.end(), subject.begin(), [](unsigned char c) { return std::tolower(c); });
 
-    push_back_grade();
+        print_vector(subject);
+        break;
+    }
+    case 3: abort; break;
+        
+    }
+
    
 }
 
 
-    /*double CalculateAverage() {
-
-        int x = 0;
-        return x;
-    }*/
+    
